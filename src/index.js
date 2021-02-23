181203b96f2e9ccdfd2ee21bb0d97e40b23b30c5
MohmadMehdiKarimi <karimimohammadmehdi99@gmail.com>
2021-02-23 11:33:30 +0300

    delete code

--- src/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('login_page').addEventListener('click', e => {
-  location.href = "../login/index.html";
-});
-
-
-document.getElementById('signup_page').addEventListener('click', e => {
-  location.href = "../signup/index.html";
-});
