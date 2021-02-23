5af805d6cd4a8319fe3dc129f498449019971bdd
MohmadMehdiKarimi <karimimohammadmehdi99@gmail.com>
2021-02-23 11:36:23 +0300

    make header section

---
+++ src/pages/product/index.js
@@ -0,0 +1,8 @@
+document.getElementById('signIn-page').addEventListener('click', e => {
+    location.href = "../login/index.html";
+  });
+  
+  
+  document.getElementById('register-page').addEventListener('click', e => {
+    location.href = "../signup/index.html";
+  });
